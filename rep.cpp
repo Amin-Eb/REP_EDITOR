cce29a9e3c937057d2e7d79f9e226aa5d3a28935
Amin-Eb <mohamadaminebadi08@gmail.com>
2024-09-30 11:40:28 +0330

    using ncurses

--- rep.cpp
+++ rep.cpp
@@ -1,16 +1,47 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-
+#include <ncurses.h>
+#include <stdio.h>
 using namespace std;
 
+void init();
+
+char ch;
+int row, col, y = 0, x = 0;
 
 int main(int argc,  char** argv){
-	ifstream file (argv[1]);
-	if(!file.is_open()) return cout << "Not such a file!" << endl, 0;
-	
+	init();
+    ifstream file (argv[1]);
+    if(!file.is_open()) printw("Not such a file!\n");
 	string line;
-	while( getline(file,line) ) cout << line << endl;
-   
-    file.close();
+	while( getline(file,line) ) printw("%s\n", line.c_str());
+	mvchgat(y, x, -1, A_BLINK, 1, NULL);	
+	refresh();
+
+	while(true){
+		ch = getch();
+	//	printw("%d",ch);
+		if(ch == 4){//left
+			if(x > 0) x --;
+			else if(x == 0) y = max(0, y -1), x = row - 1;
+			move(y, x);
+		}
+		if(ch == 5){//right
+			if(x < row - 1) x ++;
+			else if(x == row- 1) y ++, x = 0;
+			move(y, x);
+		}
+		refresh();
+	}
+	getch();
+	endwin();
+
+}
+
+void init(){
+	initscr();
+	noecho(); //   do not show the given characters from input
+	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
+	getmaxyx(stdscr,row,col);
 }
