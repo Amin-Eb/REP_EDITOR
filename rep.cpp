4d2d3964fc39fbc22eab52f8413335f4aaa58740
Amin-Eb <mohamadaminebadi08@gmail.com>
2024-12-15 20:22:50 +0330

    mouse function improved

--- rep.cpp
+++ rep.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <string>
 #include <dlfcn.h>
+#include <chrono>
 #include <tree_sitter/api.h>
 #include <nlohmann/json.hpp>
 #include "src/Syntax.h"
@@ -54,28 +55,16 @@ int main(int argc, char** argv) {
             RepScreen.PrintScr(Rep);
             move(y, x);
             mode = 0;
+            continue;
         }
         if(ch == KEY_MOUSE) {
             if(getmouse(&event) == OK){
+                auto TimeStart = chrono::high_resolution_clock::now();
                 if(event.bstate == 65536)
                     RepNormal.SendKey(1, CurrentLine, x, y, RepScreen, Rep);
                 if(event.bstate == 2097152)
                     RepNormal.SendKey(2, CurrentLine, x, y, RepScreen, Rep);
-                if(event.bstate & BUTTON1_CLICKED){
-                    y = event.y;
-                    x = event.x;
-                    CurrentLine = RepScreen.TheStart + y;
-                    if(event.x < 4)
-                        x = 4;
-                    if(event.x > Rep.Matn[CurrentLine].size() + 4)
-                        x = Rep.Matn[CurrentLine].size() + 4;
-                    move(y, x);
-                    refresh();
-                    continue;
-                }
                 if(event.bstate & BUTTON1_PRESSED){
-                    if(event.x < 4 || event.x > Rep.Matn[CurrentLine].size() + 4)
-                        continue;
                     ch = getch();
                     y2 = event.y;
                     x2 = event.x;
@@ -87,19 +76,37 @@ int main(int argc, char** argv) {
                     while(true){
                         if(getmouse(&event) == OK){
                             if(event.bstate & BUTTON1_RELEASED){  
-                                y = event.y;
-                                x = event.x;
-                                int TCurrentLine = RepScreen.TheStart + y;
-                                if(event.x < 4)
-                                    x = 4;
-                                if(event.x > Rep.Matn[TCurrentLine].size() + 4)
-                                    x = Rep.Matn[TCurrentLine].size() + 4;
-                                RepScreen.Highlight(y2, CurrentLine,x2 , y2, x, y, Rep);
-                                break;
+                                auto TimeEnd = chrono::high_resolution_clock::now();
+                                auto ElapsedTime = chrono::duration<double, std::milli>(TimeEnd - TimeStart).count();
+                                if(ElapsedTime < 200){
+                                    y = event.y;
+                                    x = event.x;
+                                    CurrentLine = RepScreen.TheStart + y;
+                                    if(event.x < 4)
+                                        x = 4;
+                                    if(event.x > Rep.Matn[CurrentLine].size() + 4)
+                                        x = Rep.Matn[CurrentLine].size() + 4;
+                                    move(28, 0);
+                                    move(y, x);
+                                    refresh();
+                                    break;
+                                }
+                                else{
+                                    mode = 2;
+                                    y = event.y;
+                                    x = event.x;
+                                    int TCurrentLine = RepScreen.TheStart + y;
+                                    if(event.x < 4)
+                                        x = 4;
+                                    if(event.x > Rep.Matn[TCurrentLine].size() + 4)
+                                        x = Rep.Matn[TCurrentLine].size() + 4;
+                                    RepScreen.Highlight(y2, CurrentLine,x2 , y2, x, y, Rep);
+                                    break;
+                                }
                             }
                         }
                     }
-                    mode = 2;
+                    
                 }
                 continue;
             }
