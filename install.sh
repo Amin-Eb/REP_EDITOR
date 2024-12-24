a76dabcb15296fab342eafce1f887124f4e6fc6d
Amin-Eb <mohamadaminebadi08@gmail.com>
2024-12-24 22:11:17 +0330

    bug fixed

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 pip install conan
-sudo conan profile detect
-sudo conan install . --output-folder=build --build=missing -s compiler.cppstd=20
-sudo chmod -R 777 build
+conan profile detect
+sudo proxychains4 conan install . --output-folder=build --build=missing -s compiler.cppstd=20
+chmod +x build
 mv color.json build/color.json
 mv tree-sitter-cpp.so build/tree-sitter-cpp.so
 cd build
