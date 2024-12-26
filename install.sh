1b066dfcce3a617b4e3e5e070dabdab9b73fc6c2
Amin-Eb <mohamadaminebadi08@gmail.com>
2024-12-26 17:00:48 +0330

    CI

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
 pip install conan
-conan profile detect
-conan install . --output-folder=build --build=missing -s compiler.cppstd=20
-chmod +x build
-mv color.json build/color.json
-mv tree-sitter-cpp.so build/tree-sitter-cpp.so
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-sudo make
+conan --version
