4b9c8f02b10198cf071f74492a306994f47ba62f
Amin-Eb <mohamadaminebadi08@gmail.com>
2024-12-28 00:26:18 +0330

    Editor tests

--- tests/Editor.cpp
+++ tests/Editor.cpp
@@ -3,12 +3,20 @@
 
 TEST(EditorTest, InsertText) {
     Editor editor;
-    editor.AddLine(0,"salamo");
-    EXPECT_EQ(editor.Matn[0], "salamo");
+    editor.AddLine(0,"salam");
+    EXPECT_EQ(editor.Matn[0], "salam");
+    editor.AddCharacter(0, 1, 'c');
+    EXPECT_EQ(editor.Matn[0], "scalam");
 }
-
+TEST(EditorTest, DeleteText) {
+    Editor editor;
+    editor.AddLine(0,"salam");
+    editor.DeleteCharacter(0,2);
+    EXPECT_EQ(editor.Matn[0],"saam");
+    editor.DeleteLine(0);
+    EXPECT_EQ(editor.Matn[0], "");
+}
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
