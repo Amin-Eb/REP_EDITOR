23e0ab800a53390cf619912212ff8d62c8627550
Aminebadi <75445071+Amin-Eb@users.noreply.github.com>
2024-12-27 10:13:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 pip install --upgrade --force-reinstall conan
-conan --version
+conan profile detect
+conan install . --output-folder=build --build=missing -s compiler.cppstd=20
+chmod +x build
+mv color.json build/color.json
+mv tree-sitter-cpp.so build/tree-sitter-cpp.so
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+sudo make
