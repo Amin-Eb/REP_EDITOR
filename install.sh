526e0fc514a1d35eaf733cd26995336195885dbb
Amin-Eb <mohamadaminebadi08@gmail.com>
2025-01-11 00:42:03 +0330

    installing conan in multiple devices

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-pip install --upgrade --force-reinstall conan
-conan profile detect
-conan install . --output-folder=build --build=missing -s compiler.cppstd=20
+sudo apt install pipx
+sudo pipx ensurepath --force
+pipx install --force conan
+/root/.local/bin/conan profile detect
+/root/.local/bin/conan install . --output-folder=build --build=missing -s compiler.cppstd=20
 chmod +x build
 mv color.json build/color.json
 mv tree-sitter-cpp.so build/tree-sitter-cpp.so
