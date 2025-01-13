1ad9f50d9d7e75552bbb99d81b73e0f50d3cd4d9
Amin-Eb <mohamadaminebadi08@gmail.com>
2025-01-13 19:13:00 +0330

    install.sh file updated

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 sudo apt install pipx
-sudo pipx ensurepath --force
 pipx install --force conan
-sudo conan profile detect
-sudo conan install . --output-folder=build --build=missing -s compiler.cppstd=20
-chmod 777 build
-mv color.json build/color.json
-mv tree-sitter-cpp.so build/tree-sitter-cpp.so
+pipx ensurepath
+conan profile detect
+conan install . --output-folder=build --build=missing -s compiler.cppstd=20
+sudo chmod +x build
+sudo mv color.json build/color.json
+sudo mv tree-sitter-cpp.so build/tree-sitter-cpp.so
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+sudo cmake .. -DCMAKE_BUILD_TYPE=Release
 sudo make
